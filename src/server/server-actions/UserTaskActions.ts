33da6e7662030825e3c3b869134101923474e439
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2023-07-14 15:40:49 +0200

    cleanup

--- src/server/server-actions/UserTaskActions.ts
+++ src/server/server-actions/UserTaskActions.ts
@@ -1,4 +1,4 @@
-import type { DataModels, EngineClient } from '@5minds/processcube_engine_client';
+import type { EngineClient } from '@5minds/processcube_engine_client';
 import { Client } from '../lib/internal/EngineClient';
 
 export async function startProcess(
@@ -11,9 +11,4 @@ export async function finishUserTask(
   ...args: Parameters<typeof EngineClient.prototype.userTasks.finishUserTask>
 ): ReturnType<typeof EngineClient.prototype.userTasks.finishUserTask> {
   await Client.userTasks.finishUserTask(...args);
-}
-
-// for test purposes - should be removed before merge
-export function getIdentity(): DataModels.Iam.Identity {
-  return Client.userTasks.identity;
 }
