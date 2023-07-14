ea323e00c2bdd0e1a50ba29d332bfbc5843a38c1
Alexander Kasten <alexander.kasten@5minds.de>
2023-07-14 14:10:41 +0200

    Add getIdentity

---
+++ src/server/lib/getIdentity.ts
@@ -0,0 +1,18 @@
+import { cookies, headers } from 'next/headers';
+import { getToken } from 'next-auth/jwt';
+import { DataModels } from '@5minds/processcube_engine_client';
+
+export async function getIdentity(): Promise<DataModels.Iam.Identity> {
+  const token = await getToken({
+    req: { cookies: cookies(), headers: headers() },
+  } as any);
+
+  if (!token?.accessToken || !token?.sub) {
+    throw new Error('AccessToken or Sub could not be determined!');
+  }
+
+  return {
+    token: token?.accessToken,
+    userId: token?.sub,
+  };
+}
--- src/server/lib/index.ts
+++ src/server/lib/index.ts
@@ -1,1 +1,2 @@
 export * from './UserTaskFunctions';
+export * from './getIdentity';
