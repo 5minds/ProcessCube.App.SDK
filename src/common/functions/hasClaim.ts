a3f7e601e666031bb9affbd451bc48833942285a
Alexander Kasten <alexander.kasten@5minds.de>
2023-07-14 14:11:52 +0200

    Add hasClaim, authConfigJwtCallback and authConfigSessionCallback

--- src/common/functions/hasClaim.ts
+++ src/common/functions/hasClaim.ts
@@ -1,18 +1,58 @@
+import { Session, getServerSession, CallbacksOptions } from 'next-auth';
+import { getSession } from 'next-auth/react';
+import type { JWT } from 'next-auth/jwt';
+
 import jwtDecode from 'jwt-decode';
-import { getServerSession } from 'next-auth';
 
-async function hasClaim(claim: string): Promise<boolean> {
-  const user = (await getServerSession())?.user as any;
+export async function hasClaim(claim: string): Promise<boolean> {
+  let user: Session['user'] | undefined;
+  if (typeof window === 'undefined') {
+    user = (
+      await getServerSession({
+        callbacks: {
+          jwt: authConfigJwtCallback,
+          session: authConfigSessionCallback,
+        },
+      })
+    )?.user;
+  } else {
+    user = (await getSession())?.user;
+  }
 
-  if (!user || !user.access_token || user.access_token === '') {
+  if (!user || !user.claims) {
     return false;
   }
 
-  const decodedAccessToken = jwtDecode<Record<string, unknown>>(user.access_token);
+  return user.claims[claim] != undefined;
+}
 
-  if (!decodedAccessToken) {
-    return false;
+export async function authConfigJwtCallback(args: Parameters<CallbacksOptions['jwt']>[0]): Promise<JWT> {
+  const { token, account } = args;
+
+  if (account) {
+    token.accessToken = account.access_token;
+    token.idToken = account.id_token;
   }
+  return token;
+}
 
-  return decodedAccessToken[claim] != undefined;
+export async function authConfigSessionCallback(args: Parameters<CallbacksOptions['session']>[0]): Promise<Session> {
+  const { session, token } = args;
+  const accessToken = await decodeJwt(token.accessToken!);
+  const idToken = await decodeJwt(token.idToken!);
+
+  const idTokenKeys = Object.keys(idToken);
+  const claims = Object.fromEntries(Object.entries(accessToken).filter(([key, value]) => !idTokenKeys.includes(key)));
+
+  delete claims.scope;
+  delete claims.jti;
+  delete claims.client_id;
+
+  session.user.claims = claims;
+
+  return session;
+}
+
+async function decodeJwt(token: string) {
+  return jwtDecode<Record<string, unknown>>(token);
 }
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,1 +1,28 @@
 export * from './components/RemoteUserTask';
+export * from './functions/hasClaim';
+
+import NextAuth, { DefaultSession } from 'next-auth';
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's identity claims. */
+      claims?: Record<string, unknown>;
+    } & DefaultSession['user'];
+  }
+}
+
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID Access Token */
+    accessToken?: string;
+    /** OpenID ID Token */
+    idToken?: string;
+  }
+}
