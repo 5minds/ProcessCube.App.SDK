f0ac3110b059706e37818acdf12e4d5d0ef0965a
Alexander Kasten <alexander.kasten@5minds.de>
2023-07-18 15:25:11 +0200

    Add comments

--- src/common/functions/hasClaim.ts
+++ src/common/functions/hasClaim.ts
@@ -1,9 +1,13 @@
-import { Session, getServerSession, CallbacksOptions } from 'next-auth';
+import { Session, getServerSession, CallbacksOptions, Account } from 'next-auth';
 import { getSession } from 'next-auth/react';
 import type { JWT } from 'next-auth/jwt';
 
 import jwtDecode from 'jwt-decode';
 
+/**
+ *
+ * @returns {Boolean} A boolean whether the user has the claim or not
+ */
 export async function hasClaim(claim: string): Promise<boolean> {
   let user: Session['user'] | undefined;
   if (typeof window === 'undefined') {
@@ -26,6 +30,13 @@ export async function hasClaim(claim: string): Promise<boolean> {
   return user.claims[claim] != undefined;
 }
 
+/**
+ *
+ * This function passes the {@link Account}'s {@link Account.access_token} and {@link Account.id_token} to the generated {@link JWT}.
+ *
+ * @param args The arguments of {@link CallbacksOptions.jwt}.
+ * @returns A {@link JWT}
+ */
 export async function authConfigJwtCallback(args: Parameters<CallbacksOptions['jwt']>[0]): Promise<JWT> {
   const { token, account } = args;
 
@@ -36,6 +47,13 @@ export async function authConfigJwtCallback(args: Parameters<CallbacksOptions['j
   return token;
 }
 
+/**
+ *
+ * This function passes the {@link Account.access_token} Claims to the current {@link Session} user.
+ *
+ * @param args The arguments of {@link CallbacksOptions.session}.
+ * @returns A {@link JWT}
+ */
 export async function authConfigSessionCallback(args: Parameters<CallbacksOptions['session']>[0]): Promise<Session> {
   const { session, token } = args;
   const accessToken = await decodeJwt(token.accessToken!);
