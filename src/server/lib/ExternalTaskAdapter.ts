f167c45ef40dd3cab26dd2d28570379e44a871c5
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2023-07-18 13:36:16 +0200

    use exported config if available

--- src/server/lib/ExternalTaskAdapter.ts
+++ src/server/lib/ExternalTaskAdapter.ts
@@ -5,12 +5,13 @@ import { Engine_URL } from './internal/EngineClient';
 import path from 'path';
 import { getDirectories } from './utils';
 
-const logger = new Logger('external_task_worker_playground');
-const defaultExternalTaskWorkerConfig: IExternalTaskWorkerConfig = {
+const DEFAULT_EXTERNAL_TASK_WORKER_CONFIG: IExternalTaskWorkerConfig = {
   lockDuration: 20000,
   maxTasks: 5,
   longpollingTimeout: 1000,
 };
+
+const logger = new Logger('external_task_worker_playground');
 
 export async function subscribeToExternalTasks(external_tasks_dir: string): Promise<ExternalTaskWorker<any, any>[]> {
   logger.info(`Subscribing to external tasks at ${external_tasks_dir}`);
@@ -30,18 +31,25 @@ export async function subscribeToExternalTasks(external_tasks_dir: string): Prom
 
     const fullWorkerFilePath = path.join(directory, workerFile);
     const module = await import(fullWorkerFilePath);
-    const handler = module.default;
-
-    logger.info(`Found external task handler '${handler.name}' in file '${fullWorkerFilePath}'`);
 
     const identity = await getIdentity();
+    const lockDuration = (await module.lockDuration) ?? DEFAULT_EXTERNAL_TASK_WORKER_CONFIG.lockDuration;
+    const maxTasks = (await module.maxTasks) ?? DEFAULT_EXTERNAL_TASK_WORKER_CONFIG.maxTasks;
+    const longpollingTimeout =
+      (await module.longpollingTimeout) ?? DEFAULT_EXTERNAL_TASK_WORKER_CONFIG.longpollingTimeout;
 
-    const config: IExternalTaskWorkerConfig = {
-      ...defaultExternalTaskWorkerConfig,
-      identity: identity,
-    };
+    logger.info(`Using lock duration of ${lockDuration}ms`);
+    logger.info(`Using max tasks of ${maxTasks}`);
+    logger.info(`Using longpolling timeout of ${longpollingTimeout}ms`);
 
     const topic = path.basename(directory);
+    const handler = module.default;
+    const config: IExternalTaskWorkerConfig = {
+      lockDuration: lockDuration,
+      maxTasks: maxTasks,
+      longpollingTimeout: longpollingTimeout,
+      identity: identity,
+    };
     const externalTaskWorker = new ExternalTaskWorker<any, any>(Engine_URL, topic, handler, config);
 
     logger.info(`Starting external task worker ${externalTaskWorker.workerId} for topic '${topic}'`);
