8de318ea8aa459a878c9f7efba8e9a6bb2dbdc0e
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2023-07-20 14:24:42 +0200

    cleanup

--- src/server/lib/ExternalTaskAdapter.ts
+++ src/server/lib/ExternalTaskAdapter.ts
@@ -5,7 +5,6 @@ import { Engine_URL } from './internal/EngineClient';
 import path from 'path';
 import esbuild from 'esbuild';
 import { getDirectories } from './utils';
-import fs from 'fs';
 
 const DEFAULT_EXTERNAL_TASK_WORKER_CONFIG: IExternalTaskWorkerConfig = {
   lockDuration: 20000,
@@ -16,8 +15,6 @@ const DEFAULT_EXTERNAL_TASK_WORKER_CONFIG: IExternalTaskWorkerConfig = {
 const logger = new Logger('external_task_worker_playground');
 
 export async function subscribeToExternalTasks(external_tasks_dir: string): Promise<ExternalTaskWorker<any, any>[]> {
-  logger.info(`Subscribing to external tasks at ${external_tasks_dir}`);
-
   const allExternalTaskWorker = [];
   const directories = await getDirectories(external_tasks_dir);
 
@@ -29,12 +26,11 @@ export async function subscribeToExternalTasks(external_tasks_dir: string): Prom
       continue;
     }
 
-    logger.info(`Found worker file in directory '${directory}'`);
-
     const fullWorkerFilePath = path.join(directory, workerFile);
     await importAndTranspile(fullWorkerFilePath);
     const pathToModule = path.join(directory, 'dist', 'worker.js');
 
+    // TODO: find a better way to import the module?
     let module = await import(pathToModule);
     if (module.default.default) {
       module = module.default;
@@ -46,14 +42,8 @@ export async function subscribeToExternalTasks(external_tasks_dir: string): Prom
     const longpollingTimeout =
       (await module.longpollingTimeout) ?? DEFAULT_EXTERNAL_TASK_WORKER_CONFIG.longpollingTimeout;
 
-    // logger.info(`Using lock duration of ${lockDuration}ms`);
-    // logger.info(`Using max tasks of ${maxTasks}`);
-    // logger.info(`Using longpolling timeout of ${longpollingTimeout}ms`);
-
     const topic = path.basename(directory);
     const handler = module.default;
-
-    logger.info(`Using handler ${handler} ${JSON.stringify(handler)} with type ${typeof handler}`);
 
     const config: IExternalTaskWorkerConfig = {
       lockDuration: lockDuration,
@@ -91,10 +81,6 @@ async function getIdentity(): Promise<Identity> {
 }
 
 async function importAndTranspile(fullWorkerFilePath: string) {
-  // const tsCode = await fs.promises.readFile(fullWorkerFilePath, 'utf-8');
-  // logger.info(`Read code from file '${fullWorkerFilePath}'`);
-  // logger.info(`Code: ${tsCode}`);
-
   const result = await esbuild.build({
     entryPoints: [fullWorkerFilePath],
     outfile: path.join(path.dirname(fullWorkerFilePath), 'dist', 'worker.js'),
