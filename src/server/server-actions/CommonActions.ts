cda56e89d289ad281d78ebad24df1a008ecec6e1
Matthias Danne <matthias.danne@5minds.de>
2023-07-21 08:56:08 +0200

    Add backupUrl in case of invalid navigation target

--- src/server/server-actions/CommonActions.ts
+++ src/server/server-actions/CommonActions.ts
@@ -14,33 +14,62 @@ export async function hardNavigate(url: string, type?: RedirectType) {
   redirect(url, type);
 }
 
-export async function navigateToNextUserTaskInProcess(processInstanceId: string): Promise<void> {
+/**
+ * Navigates to the next UserTask in the given ProcessInstance. If there is no UserTask, the user will be redirected to the backupUrl. If no backupUrl is given, the app crashes if there is no UserTask.
+ * @param processInstanceId ID of the ProcessInstance
+ * @param backupUrl The url to redirect to, if there is no UserTask
+ * @returns Promise<void>
+ */
+export async function navigateToNextUserTaskInProcess(processInstanceId: string, backupUrl?: string): Promise<void> {
   const userTask: DataModels.FlowNodeInstances.UserTaskInstance | null = await getWaitingUserTaskByProcessInstanceId(processInstanceId);
 
   if (userTask === null) {
-    throw new Error(`No suspended FlowNodeInstance found for ProcessInstance with ID ${processInstanceId}`);
+    if (backupUrl) {
+      redirect(backupUrl);
+    }
+
+    return;
   }
 
   await navigateToUserTaskInstance(userTask);
 }
 
-export async function navigateToNextUserTaskInCorrelation(correlationId: string): Promise<void> {
+/**
+ * Navigates to the next UserTask in the given Correlation. If there is no UserTask, the user will be redirected to the backupUrl. If no backupUrl is given, the app crashes if there is no UserTask.
+ * @param backupUrl The url to redirect to, if there is no UserTask
+ * @returns Promise<void>
+ */
+export async function navigateToNextUserTaskInCorrelation(correlationId: string, backupUrl?: string): Promise<void> {
   const userTask: DataModels.FlowNodeInstances.UserTaskInstance | null = await getWaitingUserTaskByCorrelationId(
     correlationId
   );
 
   if (userTask === null) {
-    throw new Error(`No suspended UserTaskInstance found for Correlation with ID ${correlationId}`);
+    if (backupUrl) {
+      redirect(backupUrl);
+    }
+
+    return;
   }
 
   await navigateToUserTaskInstance(userTask);
 }
 
-export async function navigateToNextUserTaskOfSameType(flowNodeId: string): Promise<void> {
+/**
+ * Navigates to the next UserTask of the same FlowNodeId. If there is no UserTask, the user will be redirected to the backupUrl. If no backupUrl is given, the app crashes if there is no UserTask.
+ * @param flowNodeId ID of the FlowNode
+ * @param backupUrl The url to redirect to, if there is no UserTask
+ * @returns Promise<void>
+ */
+export async function navigateToNextUserTaskOfSameFlowNodeId(flowNodeId: string, backupUrl?: string): Promise<void> {
   const userTaskList: DataModels.FlowNodeInstances.UserTaskList = await getWaitingUserTasksByFlowNodeId(flowNodeId);
 
   if (userTaskList.userTasks.length === 0) {
-    throw new Error(`No suspended FlowNodeInstance found for FlowNodeId ${flowNodeId}`);
+    if (backupUrl) {
+      redirect(backupUrl);
+    }
+
+    return;
   }
 
   const userTask: DataModels.FlowNodeInstances.UserTaskInstance = userTaskList.userTasks[0];
@@ -58,7 +87,7 @@ async function navigateToUserTaskInstance(userTaskInstance: DataModels.FlowNodeI
   const processInstanceId: string = userTaskInstance.processInstanceId;
   const flowNodeId: string = userTaskInstance.flowNodeId;
 
-  const uri: string = `/${processModelId}/${processInstanceId}/${flowNodeId}`;
+  const url: string = `/${processModelId}/${processInstanceId}/${flowNodeId}`;
 
-  redirect(uri);
+  redirect(url);
 }
