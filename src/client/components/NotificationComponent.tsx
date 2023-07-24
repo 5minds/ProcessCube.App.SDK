8e6b575a1ed998b6d65dc723d494c205635c78ff
Matthias Danne <matthias.danne@5minds.de>
2023-07-24 11:48:01 +0200

    Simple NotificationComponent example

---
+++ src/client/components/NotificationComponent.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { FiBell } from 'react-icons/fi';
+import { IconButton } from '@chakra-ui/react';
+
+export const NotificationComponent = () => {
+  const [showNotifications, setShowNotifications] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(sendNotification, 3000);
+
+    return () => clearInterval(interval);
+  }, [showNotifications]);
+
+  const sendNotification = () => {
+    if (showNotifications) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          new window.Notification('Hello!', {
+            body: 'This is a notification sent via the Notification API.',
+          });
+          console.log('Notification sent!');
+        }
+      });
+    }
+  };
+
+  return (
+    <IconButton
+      icon={<FiBell fontSize="1.25rem" />}
+      aria-label="Settings"
+      onClick={() => setShowNotifications(!showNotifications)}
+      style={{ color: showNotifications ? 'red' : 'white' }}
+    />
+  );
+};
--- src/client/index.ts
+++ src/client/index.ts
@@ -2,3 +2,4 @@
 import 'client-only';
 
 export * from './components/DynamicLink';
+export * from './components/NotificationComponent';
