d5832a807fcdee36465f9c67cd7ba708b6b5692e
Matthias Danne <matthias.danne@5minds.de>
2023-07-28 15:14:04 +0200

    Wrap NotificationIcon.tsx in own ChakraProvider

--- src/client/components/NotificationIcon.tsx
+++ src/client/components/NotificationIcon.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FiBell, FiBellOff } from 'react-icons/fi';
-import { IconButton, Icon, Box, Badge, Flex, Text, Popover, Button, PopoverHeader, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody, PopoverTrigger, ButtonGroup, PopoverFooter } from '@chakra-ui/react';
+import { IconButton, Icon, Box, Badge, Flex, Text, Popover, Button, PopoverHeader, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody, PopoverTrigger, ButtonGroup, PopoverFooter, ChakraProvider } from '@chakra-ui/react';
 
 import useSWR from 'swr';
 
@@ -21,56 +21,56 @@ export const NotificationIcon = () => {
   if (!data) return <div>Loading...</div>;
 
   return (
-    // <Flex position="relative" align="center">
-    //   <IconButton
-    //     icon={<FiBell fontSize="1.25rem" /> }
-    //     aria-label="Settings"
-    //     onClick={() => console.log('clicked')}
-    //   />
-    //   {notificationCount > 0 && (
-    //     <Box
-    //       bg="red"
-    //       w="16px"
-    //       h="16px"
-    //       borderRadius="50%"
-    //       position="absolute"
-    //       top="-8px"
-    //       right="-8px"
-    //       display="flex"
-    //       justifyContent="center"
-    //       alignItems="center"
-    //       fontSize="0.8rem"
-    //       color="white"
-    //     >
-    //       {notificationCount}
-    //     </Box>
-    //   )}
-    // </Flex>
-
-    <Box>
-      <Popover placement="bottom" closeOnBlur={false}>
-        <PopoverTrigger>
-          <Button>Trigger</Button>
-        </PopoverTrigger>
-        <PopoverContent color="white" bg="blue.800" borderColor="blue.800">
-          <PopoverHeader pt={4} fontWeight="bold" border="0">
-            Manage Your Channels
-          </PopoverHeader>
-          <PopoverArrow bg="blue.800" />
-          <PopoverCloseButton />
-          <PopoverBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore.
-          </PopoverBody>
-          <PopoverFooter border="0" display="flex" alignItems="center" justifyContent="space-between" pb={4}>
-            <Box fontSize="sm">Step 2 of 4</Box>
-            <ButtonGroup size="sm">
-              <Button colorScheme="green">Setup Email</Button>
-              <Button colorScheme="blue">Next</Button>
-            </ButtonGroup>
-          </PopoverFooter>
-        </PopoverContent>
-      </Popover>
-    </Box>
+    <ChakraProvider>
+      <Box>
+        <Popover placement="bottom" closeOnBlur={false}>
+          <PopoverTrigger>
+            <Flex position="relative" align="center">
+              <IconButton
+                icon={<FiBell fontSize="1.25rem" /> }
+                aria-label="Settings"
+                onClick={() => console.log('clicked')}
+              />
+              {notificationCount > 0 && (
+                <Box
+                  bg="red"
+                  w="16px"
+                  h="16px"
+                  borderRadius="50%"
+                  position="absolute"
+                  top="-8px"
+                  right="-8px"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  fontSize="0.8rem"
+                  color="white"
+                >
+                  {notificationCount}
+                </Box>
+              )}
+           </Flex>
+          </PopoverTrigger>
+          <PopoverContent color="white" bg="blue.800" borderColor="blue.800">
+            <PopoverHeader pt={4} fontWeight="bold" border="0">
+              Manage Your Channels
+            </PopoverHeader>
+            <PopoverArrow bg="blue.800" />
+            <PopoverCloseButton />
+            <PopoverBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore.
+            </PopoverBody>
+            <PopoverFooter border="0" display="flex" alignItems="center" justifyContent="space-between" pb={4}>
+              <Box fontSize="sm">Step 2 of 4</Box>
+              <ButtonGroup size="sm">
+                <Button colorScheme="green">Setup Email</Button>
+                <Button colorScheme="blue">Next</Button>
+              </ButtonGroup>
+            </PopoverFooter>
+          </PopoverContent>
+        </Popover>
+      </Box>
+    </ChakraProvider>
   );
 };
