860e40bc7ceb0859c458d721aa0dbed377d98b1f
Matthias Danne <matthias.danne@5minds.de>
2023-07-31 09:48:33 +0200

    Copy/paste tasklist to popover content

--- src/client/components/NotificationIcon.tsx
+++ src/client/components/NotificationIcon.tsx
@@ -1,24 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { FiBell, FiBellOff } from 'react-icons/fi';
-import { IconButton, Icon, Box, Badge, Flex, Text, Popover, Button, PopoverHeader, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody, PopoverTrigger, ButtonGroup, PopoverFooter, ChakraProvider } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { FiBell } from 'react-icons/fi';
+import {
+  IconButton,
+  Box,
+  Flex,
+  Popover,
+  Button,
+  PopoverHeader,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverBody,
+  PopoverTrigger,
+  ButtonGroup,
+  PopoverFooter,
+  ChakraProvider,
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+} from '@chakra-ui/react';
 
 import useSWR from 'swr';
+import { DataModels } from '@5minds/processcube_engine_client';
 
 const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
 export const NotificationIcon = () => {
   const [notificationCount, setNotificationCount] = useState(0);
+  const [taskList, setTaskList] = useState([]);
 
   const { data, error } = useSWR('/api/usertasks', fetcher, {
     refreshInterval: 3000,
-    onSuccess: (data) => {
-      console.log('data', data);
-      setNotificationCount(data.length);
+    onSuccess: (taskList) => {
+      console.log('taskList', taskList);
+      setNotificationCount(taskList.length);
+      setTaskList(taskList);
     },
   });
 
+  const handleClick = (task: DataModels.FlowNodeInstances.UserTaskInstance) => {
+    console.log('handleClick', task);
+  };
+
   if (error) return <div>Error fetching data</div>;
-  if (!data) return <div>Loading...</div>;
+  // if (!data) return <div>Loading...</div>;
 
   return (
     <ChakraProvider>
@@ -26,20 +52,16 @@ export const NotificationIcon = () => {
         <Popover placement="bottom" closeOnBlur={false}>
           <PopoverTrigger>
             <Flex position="relative" align="center">
-              <IconButton
-                icon={<FiBell fontSize="1.25rem" /> }
-                aria-label="Settings"
-                onClick={() => console.log('clicked')}
-              />
+              <IconButton icon={<FiBell fontSize="1.25rem" />} aria-label="Settings" bg="gray.300" />
               {notificationCount > 0 && (
                 <Box
                   bg="red"
-                  w="16px"
-                  h="16px"
+                  w="20px"
+                  h="20px"
                   borderRadius="50%"
                   position="absolute"
-                  top="-8px"
-                  right="-8px"
+                  top="-10px"
+                  right="-10px"
                   display="flex"
                   justifyContent="center"
                   alignItems="center"
@@ -49,23 +71,40 @@ export const NotificationIcon = () => {
                   {notificationCount}
                 </Box>
               )}
-           </Flex>
+            </Flex>
           </PopoverTrigger>
           <PopoverContent color="white" bg="blue.800" borderColor="blue.800">
             <PopoverHeader pt={4} fontWeight="bold" border="0">
-              Manage Your Channels
+              Neue Aufgaben
             </PopoverHeader>
             <PopoverArrow bg="blue.800" />
             <PopoverCloseButton />
             <PopoverBody>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore.
+              <Box>
+                {taskList.map((task: DataModels.FlowNodeInstances.UserTaskInstance) => (
+                  <Box as="section" py={{ base: '4', md: '8' }} minW={{ base: '100%', md: '60%' }}>
+                    <Card
+                      variant="elevated"
+                      direction={{ base: 'column', md: 'row' }}
+                      justify={{ base: 'space-between' }}
+                      overflow="hidden"
+                    >
+                      <CardHeader>
+                        <Heading size="sm">{task.processModelId}</Heading>
+                      </CardHeader>
+                      <CardBody textAlign="right">
+                        <Button variant="primary" onClick={() => handleClick(task)}>
+                          {task.flowNodeName}
+                        </Button>
+                      </CardBody>
+                    </Card>
+                  </Box>
+                ))}
+              </Box>
             </PopoverBody>
             <PopoverFooter border="0" display="flex" alignItems="center" justifyContent="space-between" pb={4}>
-              <Box fontSize="sm">Step 2 of 4</Box>
-              <ButtonGroup size="sm">
-                <Button colorScheme="green">Setup Email</Button>
-                <Button colorScheme="blue">Next</Button>
+              <ButtonGroup size="sm" ml="auto">
+                <Button colorScheme="green">Alle Aufgaben als gelesen markieren</Button>
               </ButtonGroup>
             </PopoverFooter>
           </PopoverContent>
