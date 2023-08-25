71ddc47952a8b56223324b1a303bcd40ac137948
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2023-08-25 16:37:21 +0200

    add processModelId and options parameter

--- src/server/lib/ProcessInstanceFunctions.ts
+++ src/server/lib/ProcessInstanceFunctions.ts
@@ -2,13 +2,17 @@ import { DataModels } from '@5minds/processcube_engine_client';
 import { Client } from './internal/EngineClient';
 
 /**
- *
+ * @param processModelId The ID of the ProcessModel the ProcessInstance belongs to
  * @param options The query options of {@link Client.processInstances.query}
  * @returns {DataModels.ProcessInstances.ProcessInstance[] | null}
  */
-export async function getActiveProcessInstances(options?: Parameters<typeof Client.processInstances.query>[1]) {
+export async function getActiveProcessInstances(
+  processModelId?: string,
+  options?: Parameters<typeof Client.processInstances.query>[1]
+): Promise<DataModels.ProcessInstances.ProcessInstance[] | null> {
   const result = await Client.processInstances.query(
     {
+      processModelId: processModelId,
       state: DataModels.ProcessInstances.ProcessInstanceState.running,
     },
     options
