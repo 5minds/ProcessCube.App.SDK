05f8f9ef535c4377d45ea47782af0e4c29e7d07e
Jeremy-Hill-02 <88316345+Jeremy-Hill-02@users.noreply.github.com>
2023-08-29 09:03:52 +0200

    give user all query options except state

--- src/server/lib/ProcessInstanceFunctions.ts
+++ src/server/lib/ProcessInstanceFunctions.ts
@@ -2,20 +2,21 @@ import { DataModels } from '@5minds/processcube_engine_client';
 import { Client } from './internal/EngineClient';
 
 /**
- * @param processModelId The ID of the ProcessModel the ProcessInstance belongs to
- * @param options The query options of {@link Client.processInstances.query}
+ * @param query The query options of {@link Client.processInstances.query}
+ * @param query.query The query of {@link Client.processInstances.query}
+ * @param query.options The options of {@link Client.processInstances.query}
  * @returns {DataModels.ProcessInstances.ProcessInstance[] | null}
  */
-export async function getActiveProcessInstances(
-  processModelId?: string,
-  options?: Parameters<typeof Client.processInstances.query>[1]
-): Promise<DataModels.ProcessInstances.ProcessInstance[] | null> {
+export async function getActiveProcessInstances(query?: {
+  query?: Omit<DataModels.ProcessInstances.ProcessInstanceQuery, 'state'>;
+  options?: Parameters<typeof Client.processInstances.query>[1];
+}): Promise<DataModels.ProcessInstances.ProcessInstance[] | null> {
   const result = await Client.processInstances.query(
     {
-      processModelId: processModelId,
+      ...query?.query,
       state: DataModels.ProcessInstances.ProcessInstanceState.running,
     },
-    options
+    query?.options
   );
 
   if (result.processInstances.length === 0) {
