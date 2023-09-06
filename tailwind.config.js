19e778e2402079bee052a83c7ed046ac4c7fd84a
Marc Räder <marc.raeder@5minds.de>
2023-09-06 15:09:18 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./src/client/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
