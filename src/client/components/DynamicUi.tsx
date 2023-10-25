3aa6f5b42e600ed0c546bb404c3b1a93822dbaef
Alexander Kasten <alexander.kasten@5minds.de>
2023-10-25 17:07:34 +0200

    Add StringFormField

--- src/client/components/DynamicUi.tsx
+++ src/client/components/DynamicUi.tsx
@@ -346,3 +346,43 @@ export const ParagraphFormField: React.FC<ParagraphFormFieldProps> = ({ formFiel
 
   return paragraphContainer;
 };
+
+export interface IStringFormFieldProps {
+  formField: DataModels.FlowNodeInstances.UserTaskFormField;
+  state?: string | null;
+}
+
+export const StringFormField: React.FC<IStringFormFieldProps> = ({ formField, state }) => {
+  const parsedCustomFormConfig = parseCustomFormConfig(formField.customForm);
+
+  const label = formField.label;
+  const inputType = parsedCustomFormConfig?.multiline === 'true' ? 'textarea' : 'input';
+  const textInput = React.createElement(inputType, {
+    className:
+      'shadow-sm focus:ring-[color:var(--uic-focus-color)] focus:border-[color:var(--uic-focus-color)] block w-full sm:text-sm rounded-md border-[color:var(--uic-border-color)] invalid:border-red-500 invalid:ring-red-500 invalid:ring-1 dark:border-solid dark:border-transparent dark:bg-studio-gray-350 dark:focus:shadow-studio-dark dark:focus:border-[#007bff40] dark:focus:ring-[#007bff40] dark:placeholder-gray-400 dark:invalid:shadow-studio-dark-invalid dark:invalid:border-[#dc35467f] dark:invalid:ring-[#dc35467f]',
+    id: `${formField.id}-string`,
+    name: `${formField.id}-string`,
+    value: state || (formField.defaultValue?.toString() ?? ''),
+    placeholder: parsedCustomFormConfig?.placeholder,
+    'aria-describedby': parsedCustomFormConfig?.hint ? `${formField.id}-hint` : undefined,
+    type: inputType === 'input' ? 'text' : undefined,
+    rows: inputType === 'textarea' ? 4 : undefined,
+  });
+
+  const hint = parsedCustomFormConfig?.hint ? (
+    <p id={`${formField.id}-hint`} className="mt-2 text-sm text-gray-500 dark:text-studio-gray-200">
+      {parsedCustomFormConfig.hint}
+    </p>
+  ) : null;
+
+  return (
+    <div>
+      <label className="block text-sm font-medium" htmlFor={`${formField.id}-string`}>
+        {label}
+      </label>
+      <div className="mt-1">{textInput}</div>
+      {hint}
+    </div>
+  );
+};
+
