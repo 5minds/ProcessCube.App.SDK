3dd60eed54bdd429524ed8bf4ec089b222d890ff
Alexander Kasten <alexander.kasten@5minds.de>
2024-01-10 14:26:37 +0100

    Fix DecimalFormField and add console warning and restore state

--- src/client/components/DynamicUi/FormFields/DateFormField.tsx
+++ src/client/components/DynamicUi/FormFields/DateFormField.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import type { DataModels } from '@5minds/processcube_engine_sdk';
-
-import { DynamicUiFormFieldRef } from '../DynamicUi';
+import { DynamicUiComponentProps, DynamicUiFormFieldRef } from '../DynamicUi';
 import { parseCustomFormConfig } from '../utils/parseCustomFormConfig';
 
-export function DateFormField(
-  props: { formField: DataModels.FlowNodeInstances.UserTaskFormField },
-  ref: DynamicUiFormFieldRef,
-) {
+export function DateFormField(props: DynamicUiComponentProps<string | null>, ref: DynamicUiFormFieldRef) {
   const { formField } = props;
   const hintId = `${formField.id}-hint`;
   const parsedCustomFormConfig = parseCustomFormConfig(formField.customForm);
+
+  if (!isValidDate(formField.defaultValue)) {
+    console.warn(`[@5minds/processcube_app_sdk:DynamicUi]\t\tInvalid default value for date field "${formField.id}"`);
+  }
+
+  const defaultValue = props.state || formField.defaultValue?.toString();
 
   return (
     <div className="">
@@ -22,7 +23,7 @@ export function DateFormField(
         <input
           className="dark:bg-dynamicui-gray-350 dark:focus:shadow-dynamicui-dark dark:invalid:shadow-dynamicui-dark-invalid block w-full rounded-md border-[color:var(--uic-border-color)] shadow-sm invalid:border-red-500 invalid:ring-1 invalid:ring-red-500 focus:border-[color:var(--uic-focus-color)] focus:ring-[color:var(--uic-focus-color)] sm:text-sm dark:border-solid dark:border-transparent dark:placeholder-gray-400 dark:invalid:border-[#dc35467f] dark:invalid:ring-[#dc35467f] dark:focus:border-[#007bff40] dark:focus:ring-[#007bff40]"
           type="date"
-          value={formField.defaultValue?.toString()}
+          defaultValue={defaultValue}
           id={formField.id}
           name={formField.id}
           aria-describedby={hintId}
@@ -36,4 +37,8 @@ export function DateFormField(
       )}
     </div>
   );
+}
+
+function isValidDate(value: any) {
+  return new Date(value?.toString()).toString() !== 'Invalid Date';
 }
