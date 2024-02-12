9913f7aab22d3bb60ae2a2849f95b1dedac3d997
NiclasPodzierski <niclas.podzierski@5minds.de>
2024-02-12 13:59:19 +0100

    Fix watch scripts

--- packages/server/src/server-actions/NavigationActions.ts
+++ packages/server/src/server-actions/NavigationActions.ts
@@ -1,7 +1,6 @@
 import { revalidatePath } from 'next/cache';
 import { RedirectType } from 'next/dist/client/components/redirect';
 import { redirect } from 'next/navigation';
-import { hasClaim } from '@app_sdk_internal/common';
 
 /**
  * Navigates to an URL. This uses next's {@link redirect} function. As this is an server action, the {@link RedirectType} is 'push' only.
