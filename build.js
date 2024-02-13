cb63445ae6920971fd9e214f6fafb3283d6ff784
NiclasPodzierski <niclas.podzierski@5minds.de>
2024-02-13 13:55:36 +0100

    Cleanup build file

--- build.js
+++ build.js
@@ -1,54 +1,66 @@
 import * as esbuild from 'esbuild'
 
+const productionBuild = process.env.NODE_ENV === 'production';
+
+function getPlugin(pathToCommon) {
+  return {
+    name: 'markCommonAsExternal',
+    setup(build) {
+      build.onResolve({ filter: /^\..*\/common.*$/ }, args => {
+        return {
+          path: pathToCommon,
+          namespace: 'file',
+          external: true,
+      }})
+    },
+  }
+
+}
+
+const PRODUCTION_CONFIG = {
+  treeShaking: true,
+  splitting: true,
+  minify: true,
+  drop: ['console'],
+};
+
+const COMMON_CONFIG = {
+  bundle: true,
+  platform: 'node',
+  ...(productionBuild ? PRODUCTION_CONFIG : {})
+};
+
 /*    ESModules    */
 
+const ESMODULE_CONFIG = {
+  ...COMMON_CONFIG,
+  format: 'esm',
+  outExtension: {
+    '.js': '.mjs',
+  },
+};
+
 //common
-await esbuild.build({
-  entryPoints: ['src/common/index.ts'],
-  bundle: true,
-  outdir: 'build/common',
-  packages: 'external',
-  platform: 'node',
-  outExtension: {
-    '.js': '.mjs',
-  },
-  format: 'esm',
-});
 const moduleBuildResult = await esbuild.build({
+  ...ESMODULE_CONFIG,
   entryPoints: ['src/common/index.ts'],
-  bundle: true,
   outdir: 'build/common',
   packages: 'external',
-  platform: 'node',
-  outExtension: {
-    '.js': '.mjs',
-  },
-  format: 'esm',
   write: false,
 });
-let myPlug = {
-  name: 'plug',
-  setup(build) {
-    build.onResolve({ filter: /^\..*\/common.*$/ }, args => {
-      return {
-        path: moduleBuildResult.outputFiles[0].path,
-        namespace: 'file',
-        external: true,
-    }})
-  },
-}
+await esbuild.build({
+  ...ESMODULE_CONFIG,
+  entryPoints: ['src/common/index.ts'],
+  outdir: 'build/common',
+  packages: 'external',
+});
 
 //server
 await esbuild.build({
+  ...ESMODULE_CONFIG,
   entryPoints: ['src/server/index.ts'],
-  bundle: true,
   outdir: 'build/server',
-  platform: 'node',
-  outExtension: {
-    '.js': '.mjs',
-  },
-  format: 'esm',
-  plugins: [myPlug],
+  plugins: [getPlugin(moduleBuildResult.outputFiles[0].path)],
   external: [
     "@opentelemetry", "fsevents", "@5minds/*", "next", "next-auth", "react", "jwt-decode", "openid-client", "esbuild"
   ],
@@ -56,80 +68,50 @@ await esbuild.build({
 
 //client
 await esbuild.build({
+  ...ESMODULE_CONFIG,
   entryPoints: ['src/client/index.ts'],
-  bundle: true,
   outdir: 'build/client',
+  plugins: [getPlugin(moduleBuildResult.outputFiles[0].path)],
   packages: 'external',
-  platform: 'node',
-  outExtension: {
-    '.js': '.mjs',
-  },
-  format: 'esm',
-  plugins: [myPlug],
 });
 
 
 /*    CommonJS    */
 
-//common
-const commonBuildResult = await esbuild.build({
-  entryPoints: ['src/common/index.ts'],
-  bundle: true,
-  outbase: 'src',
-  outdir: 'build',
-  packages: 'external',
-  platform: 'node',
+const COMMONJS_CONFIG = {
+  ...COMMON_CONFIG,
   outExtension: {
     '.js': '.cjs',
   },
+  packages: 'external',
+  splitting: false,
+};
+
+//common
+const commonBuildResult = await esbuild.build({
+  ...COMMONJS_CONFIG,
+  entryPoints: ['src/common/index.ts'],
+  outdir: 'build/common',
   write: false,
 });
 await esbuild.build({
+  ...COMMONJS_CONFIG,
   entryPoints: ['src/common/index.ts'],
-  bundle: true,
-  outbase: 'src',
-  outdir: 'build',
-  packages: 'external',
-  platform: 'node',
-  outExtension: {
-    '.js': '.cjs',
-  },
+  outdir: 'build/common',
 });
-
-myPlug = {
-  name: 'plug',
-  setup(build) {
-    build.onResolve({ filter: /^\..*\/common.*$/ }, args => {
-      return {
-        path: commonBuildResult.outputFiles[0].path,
-        namespace: 'file',
-        external: true,
-    }})
-  },
-}
 
 //server
 await esbuild.build({
+  ...COMMONJS_CONFIG,
   entryPoints: ['src/server/index.ts'],
-  bundle: true,
   outdir: 'build/server',
-  packages: 'external',
-  platform: 'node',
-  outExtension: {
-    '.js': '.cjs',
-  },
-  plugins: [myPlug],
+  plugins: [getPlugin(commonBuildResult.outputFiles[0].path)],
 });
 
 //client
 await esbuild.build({
+  ...COMMONJS_CONFIG,
   entryPoints: ['src/client/index.ts'],
-  bundle: true,
   outdir: 'build/client',
-  packages: 'external',
-  platform: 'node',
-  outExtension: {
-    '.js': '.cjs',
-  },
-  plugins: [myPlug],
+  plugins: [getPlugin(commonBuildResult.outputFiles[0].path)],
 });
