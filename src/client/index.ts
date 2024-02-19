1482197158f177399fc2c28ccb8b309c5ff8e4c7
Alexander Kasten <alexander.kasten@5minds.de>
2024-02-19 17:18:43 +0100

    Export BooleanFormField

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,3 +3,5 @@ import 'client-only';
 
 export * from './components/DynamicLink';
 export * from './components/DynamicUi/DynamicUi';
+export { FormFieldComponentMap } from './components/DynamicUi/FormFields/index';
+export { BooleanFormField } from './components/DynamicUi/FormFields/BooleanFormField';
