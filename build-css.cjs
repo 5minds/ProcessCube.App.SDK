173667ac789c9508da17d5cad04293e596218dd3
NiclasPodzierski <niclas.podzierski@5minds.de>
2024-02-20 09:36:37 +0100

    Revert "WIP render in shadow dom and auto bundle CSS"

--- build-css.cjs
+++ build-css.cjs
@@ -1,53 +1,35 @@
 const fs = require('node:fs');
-const postcss = require('postcss');
+
 const esbuild = require('esbuild');
 const postCssPlugin = require('esbuild-style-plugin');
-const inlineImportPlugin = require('esbuild-plugin-inline-import');
 
 const BASE_DIRECTORY = 'src';
-const formats = {
-  esm: '.mjs',
-  cjs: '.cjs',
-};
 
-// const entryPoints = fs
-//   .readdirSync(BASE_DIRECTORY, { recursive: true })
-//   .filter((path) => path.endsWith('.css'))
-//   .map((path) => `src/${path}`);
+const entryPoints = fs
+  .readdirSync(BASE_DIRECTORY, { recursive: true })
+  .filter((path) => path.endsWith('.css'))
+  .map((path) => `src/${path}`);
 
-/** @type {import('esbuild').BuildOptions} */
 const BUILD_OPTIONS = {
-  entryPoints: ['src/client/index.ts', 'src/common/index.ts', 'src/server/index.ts'],
+  entryPoints: entryPoints,
   outdir: '.',
   bundle: true,
   minify: process.env.NODE_ENV !== 'production' ? false : true,
   entryNames: '[dir]/[name]',
   outbase: BASE_DIRECTORY,
   packages: 'external',
-  logLevel: 'verbose',
+  outExtension: { '.js': '.EMPTY' },
   plugins: [
-    // postCssPlugin({
-    //   extract: true,
-    //   postcss: {
-    //     plugins: [require('tailwindcss'), require('autoprefixer')],
-    //   },
-    // }),
-    inlineImportPlugin({
-      transform: async (content, args) => {
-        console.log('content', content);
-        console.log('args', args);
-        const result = await postcss([require('tailwindcss'), require('autoprefixer')]).process(content, {
-          from: args.path,
-        });
-
-        console.log('result', result);
-        return result.css;
+    postCssPlugin({
+      extract: true,
+      postcss: {
+        plugins: [require('tailwindcss'), require('autoprefixer')],
       },
     }),
   ],
 };
 
-// console.log(`Build CSS for ${entryPoints.join(' and\n\t')}`);
+console.log(`Build CSS for ${entryPoints.join(' and\n\t')}`);
 
 const args = process.argv.slice(2);
 if (args.includes('--watch')) {
@@ -69,11 +51,8 @@ if (args.includes('--watch')) {
       process.exit(1);
     });
 } else {
-  Object.keys(formats).forEach((key) => {
-    const extension = formats[key];
-    esbuild.build({ ...BUILD_OPTIONS, outExtension: { '.js': extension }, format: key }).catch(() => {
-      console.error(`Build error: ${error}`);
-      process.exit(1);
-    });
+  esbuild.build(BUILD_OPTIONS).catch(() => {
+    console.error(`Build error: ${error}`);
+    process.exit(1);
   });
 }
--- src/client/packages.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'inline:*.css' {
-  const content: string;
-  export default content;
-}
