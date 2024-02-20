68b1bf6f9c1038d07c1ec9492a3f0236c689592a
NiclasPodzierski <niclas.podzierski@5minds.de>
2024-02-20 09:48:33 +0100

    Remove unused code

--- src/server/index.ts
+++ src/server/index.ts
@@ -18,34 +18,3 @@ declare global {
 }
 
 hasClaim('test');
-
-// import NextAuth, { DefaultSession } from 'next-auth';
-
-// declare module 'next-auth' {
-//   /**
-//    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-//    */
-//   interface Session {
-//     user: {
-//       /** The user's identity claims. */
-//       claims?: Record<string, unknown>;
-//     } & DefaultSession['user'];
-//     error?: 'RefreshAccessTokenError';
-//   }
-// }
-
-// import { JWT } from 'next-auth/jwt';
-
-// declare module 'next-auth/jwt' {
-//   /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
-//   interface JWT {
-//     /** OpenID Access Token */
-//     accessToken?: string;
-//     /** OpenID ID Token */
-//     idToken?: string;
-//     /** OpenID Refresh Token */
-//     refreshToken?: string;
-//     expiresAt: number;
-//     error?: 'RefreshAccessTokenError';
-//   }
-// }
