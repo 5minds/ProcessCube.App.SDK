5f34cc7a552cb762e12846635f0f5a1b0f03c950
Louisa Strunk <louisa.strunk@5minds.de>
2024-06-28 09:00:48 +0200

    add different time formfields

--- src/client/components/DynamicUi/FormFields/TimeFormField.tsx
+++ src/client/components/DynamicUi/FormFields/TimeFormField.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { DynamicUiComponentProps, DynamicUiFormFieldRef } from '../DynamicUi';
 import { parseCustomFormConfig } from '../utils/parseCustomFormConfig';
 
-export function DateFormField(props: DynamicUiComponentProps<string | null>, ref: DynamicUiFormFieldRef) {
+export function TimeFormField(props: DynamicUiComponentProps<string | null>, ref: DynamicUiFormFieldRef) {
   const { formField } = props;
   const hintId = `${formField.id}-hint`;
   const parsedCustomFormConfig = parseCustomFormConfig(formField.customForm);
@@ -22,12 +22,12 @@ export function DateFormField(props: DynamicUiComponentProps<string | null>, ref
       <div className="app-sdk-mt-1">
         <input
           className="app-sdk-form-input app-sdk-text-app-sdk-inherit app-sdk-border app-sdk-py-2 app-sdk-px-3 app-sdk-block app-sdk-w-full app-sdk-rounded-md app-sdk-border-[color:var(--asdk-dui-border-color)] app-sdk-bg-[color:var(--asdk-dui-formfield-background-color)] app-sdk-placeholder-[color:var(--asdk-dui-formfield-placeholder-text-color)] app-sdk-shadow-sm invalid:app-sdk-border-[color:var(--asdk-dui-formfield-invalid-color)] invalid:app-sdk-ring-1 invalid:app-sdk-ring-[color:var(--asdk-dui-formfield-invalid-color)] focus:app-sdk-border-[color:var(--asdk-dui-focus-color)] focus:app-sdk-ring-[color:var(--asdk-dui-focus-color)] sm:app-sdk-text-sm dark:app-sdk-border-solid dark:app-sdk-border-transparent dark:invalid:app-sdk-shadow-app-sdk-dark-invalid dark:focus:app-sdk-shadow-app-sdk-dark"
-          type="date"
+          type="time"
           defaultValue={defaultValue}
           id={formField.id}
           name={formField.id}
           aria-describedby={hintId}
-          data-form-field-type="date"
+          data-form-field-type="time"
         />
       </div>
       {parsedCustomFormConfig?.hint && (
--- src/client/components/DynamicUi/FormFields/index.ts
+++ src/client/components/DynamicUi/FormFields/index.ts
@@ -9,8 +9,12 @@ import { EmailFormField } from './EmailFormField';
 import { EnumFormField } from './EnumFormField';
 import { HeaderFormField } from './HeaderFormField';
 import { IntegerFormField } from './IntegerFormField';
+import { LocalDatetimeFormField } from './LocalDatetimeFormField';
+import { MonthFormField } from './MonthFormField';
 import { ParagraphFormField } from './ParagraphFormField';
 import { StringFormField } from './StringFormField';
+import { TimeFormField } from './TimeFormField';
+import { WeekFormField } from './WeekFormField';
 
 enum MissingFormFieldType {
   paragraph = 'paragraph',
@@ -40,12 +44,16 @@ export const FormFieldComponentMap: DynamicUiFormFieldComponentMap = {
   enum: EnumFormField,
   // longs are full numbers
   long: IntegerFormField,
+  datetime: LocalDatetimeFormField,
+  month: MonthFormField,
   // numbers can be decimals
   number: DecimalFormField,
   string: StringFormField,
   paragraph: ParagraphFormField,
   header: HeaderFormField,
   confirm: ConfirmFormField,
+  time: TimeFormField,
+  week: WeekFormField,
 };
 export { BooleanFormField } from './BooleanFormField';
 export { DateFormField } from './DateFormField';
@@ -53,7 +61,11 @@ export { DecimalFormField } from './DecimalFormField';
 export { EmailFormField } from './EmailFormField';
 export { EnumFormField } from './EnumFormField';
 export { IntegerFormField } from './IntegerFormField';
+export { LocalDatetimeFormField } from './LocalDatetimeFormField';
+export { MonthFormField } from './MonthFormField';
 export { StringFormField } from './StringFormField';
 export { ParagraphFormField } from './ParagraphFormField';
 export { HeaderFormField } from './HeaderFormField';
 export { ConfirmFormField } from './ConfirmFormField';
+export { TimeFormField } from './TimeFormField';
+export { WeekFormField } from './WeekFormField';
