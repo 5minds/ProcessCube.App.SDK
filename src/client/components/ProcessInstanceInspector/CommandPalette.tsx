9ee33cda6c6fe3e99e464774471485bb2b3dd3de
stefanluth <stefan.luthardt@outlook.com>
2024-06-28 18:30:03 +0200

    Add CSS variables

--- src/client/components/ProcessInstanceInspector/CommandPalette.tsx
+++ src/client/components/ProcessInstanceInspector/CommandPalette.tsx
@@ -50,7 +50,7 @@ export function CommandPalette<T extends Entry>(props: CommandPaletteProps<T>) {
       <div className="app-sdk-fixed app-sdk-inset-0 app-sdk-z-10 app-sdk-w-screen app-sdk-overflow-y-auto app-sdk-p-4 sm:app-sdk-p-6 md:app-sdk-p-20">
         <DialogPanel
           transition
-          className="app-sdk-mx-auto app-sdk-max-w-xl app-sdk-transform app-sdk-rounded-xl app-sdk-bg-white app-sdk-p-2 app-sdk-shadow-2xl app-sdk-ring-1 app-sdk-ring-black app-sdk-ring-opacity-5 app-sdk-transition-all data-[closed]:app-sdk-scale-95 data-[closed]:app-sdk-opacity-0 data-[enter]:app-sdk-duration-300 data-[leave]:app-sdk-duration-150 data-[enter]:app-sdk-ease-out data-[leave]:app-sdk-ease-in"
+          className="app-sdk-mx-auto app-sdk-max-w-xl app-sdk-transform app-sdk-rounded-xl app-sdk-bg-[color:var(--asdk-cmdp-background-color)] app-sdk-p-2 app-sdk-shadow-2xl app-sdk-ring-1 app-sdk-ring-black app-sdk-ring-opacity-5 app-sdk-transition-all data-[closed]:app-sdk-scale-95 data-[closed]:app-sdk-opacity-0 data-[enter]:app-sdk-duration-300 data-[leave]:app-sdk-duration-150 data-[enter]:app-sdk-ease-out data-[leave]:app-sdk-ease-in"
         >
           <Combobox
             onChange={(entry: T) => {
@@ -61,7 +61,7 @@ export function CommandPalette<T extends Entry>(props: CommandPaletteProps<T>) {
           >
             <ComboboxInput
               autoFocus
-              className="app-sdk-w-[calc(100%-2rem)] app-sdk-ring-offset-2 app-sdk-rounded-md app-sdk-border-0 app-sdk-bg-gray-100 app-sdk-px-4 app-sdk-py-2.5 app-sdk-text-gray-900 sm:app-sdk-text-sm"
+              className="app-sdk-w-full app-sdk-ring-offset-2 app-sdk-rounded-md app-sdk-border-0 app-sdk-bg-[color:var(--asdk-cmdp-input-color)] app-sdk-px-4 app-sdk-py-2.5 app-sdk-text-[color:var(--asdk-cmdp-text-color)] sm:app-sdk-text-sm"
               placeholder={props.placeholder}
               onChange={(event) => setQuery(event.target.value)}
               onBlur={() => setQuery('')}
@@ -70,7 +70,7 @@ export function CommandPalette<T extends Entry>(props: CommandPaletteProps<T>) {
             {filteredEntries.length > 0 && (
               <ComboboxOptions
                 static
-                className="app-sdk-mb-2 app-sdk-max-h-72 app-sdk-scroll-py-2 app-sdk-overflow-y-auto app-sdk-py-2 app-sdk-text-sm app-sdk-text-gray-800"
+                className="app-sdk-mb-2 app-sdk-max-h-72 app-sdk-scroll-py-2 app-sdk-overflow-y-auto app-sdk-py-2 app-sdk-text-sm app-sdk-text-[color:var(--asdk-cmdp-text-color)]"
               >
                 {filteredEntries.map((entry) => (
                   <ComboboxOption
@@ -95,7 +95,7 @@ export function CommandPalette<T extends Entry>(props: CommandPaletteProps<T>) {
                   className="app-sdk-mx-auto app-sdk-h-6 app-sdk-w-6 app-sdk-text-gray-400"
                   aria-hidden="true"
                 />
-                <p className="app-sdk-mt-4 app-sdk-text-sm app-sdk-text-gray-900">
+                <p className="app-sdk-mt-4 app-sdk-text-sm app-sdk-text-[color:var(--asdk-cmdp-text-color)]">
                   No entries found using that search term.
                 </p>
               </div>
