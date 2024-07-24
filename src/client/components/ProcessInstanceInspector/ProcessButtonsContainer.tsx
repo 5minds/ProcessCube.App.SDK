fd66f86e786e8026020431794d0bdd45ad5f6817
stefanluth <stefan.luthardt@outlook.com>
2024-07-24 15:47:31 +0800

    Fix pointer events

--- src/client/components/ProcessInstanceInspector/ProcessButtonsContainer.tsx
+++ src/client/components/ProcessInstanceInspector/ProcessButtonsContainer.tsx
@@ -2,8 +2,8 @@ import React, { PropsWithChildren } from 'react';
 
 export function ProcessButtonsContainer({ children }: PropsWithChildren) {
   return (
-    <div className="app-sdk-w-full app-sdk-absolute app-sdk-top-0 app-sdk-left-0 app-sdk-z-20 app-sdk-pt-2">
-      <div className="app-sdk-flex app-sdk-w-fit app-sdk-mx-auto app-sdk-rounded-full app-sdk-p-2 app-sdk-gap-1 app-sdk-bg-black/85">
+    <div className="app-sdk-w-full app-sdk-absolute app-sdk-top-0 app-sdk-left-0 app-sdk-z-20 app-sdk-pt-2 app-sdk-pointer-events-none">
+      <div className="app-sdk-flex app-sdk-w-fit app-sdk-mx-auto app-sdk-rounded-full app-sdk-p-2 app-sdk-gap-1 app-sdk-bg-black/85 app-sdk-pointer-events-auto">
         {children}
       </div>
     </div>
