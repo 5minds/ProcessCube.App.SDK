1a7796e1c7a6771704e69c16eb4025532d6b4fe8
stefanluth <stefan.luthardt@outlook.com>
2024-07-25 14:49:09 +0800

    Increase z-index

--- src/client/components/ProcessInstanceInspector/ProcessButtonsContainer.tsx
+++ src/client/components/ProcessInstanceInspector/ProcessButtonsContainer.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react';
 
 export function ProcessButtonsContainer({ children }: PropsWithChildren) {
   return (
-    <div className="app-sdk-w-full app-sdk-absolute app-sdk-top-0 app-sdk-left-0 app-sdk-z-20 app-sdk-pt-2 app-sdk-pointer-events-none">
+    <div className="app-sdk-w-full app-sdk-absolute app-sdk-top-0 app-sdk-left-0 app-sdk-z-50 app-sdk-pt-2 app-sdk-pointer-events-none">
       <div className="app-sdk-flex app-sdk-w-fit app-sdk-mx-auto app-sdk-rounded-full app-sdk-p-2 app-sdk-gap-1 app-sdk-bg-black/85 app-sdk-pointer-events-auto">
         {children}
       </div>
