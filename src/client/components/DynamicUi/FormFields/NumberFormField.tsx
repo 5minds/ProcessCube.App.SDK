e0b731382b7489b32debf1e4eb6abee1d606a30a
Louisa Strunk <louisa.strunk@5minds.de>
2024-07-29 10:07:15 +0200

    delete unused code

--- src/client/components/DynamicUi/FormFields/NumberFormField.tsx
+++ src/client/components/DynamicUi/FormFields/NumberFormField.tsx
@@ -11,42 +11,6 @@ export function NumberFormField(props: DynamicUiComponentProps<string | null>, r
   if (!isNumber(formField.defaultValue)) {
     console.warn(`[@5minds/processcube_app_sdk:DynamicUi]\t\tInvalid default value for number field "${formField.id}"`);
   }
-  console.log('Step', parsedCustomFormConfig?.step ?? 'kein Step eingetragen');
-
-  // let numberInput: JSX.Element;
-
-  // switch (parsedCustomFormConfig?.displayAs) {
-  //   case 'decimal':
-  //     numberInput = (
-  //       <input
-  //         className="app-sdk-form-input app-sdk-text-app-sdk-inherit app-sdk-border app-sdk-py-2 app-sdk-px-3 app-sdk-block app-sdk-w-full app-sdk-rounded-md app-sdk-border-[color:var(--asdk-dui-border-color)] app-sdk-bg-[color:var(--asdk-dui-formfield-background-color)] app-sdk-placeholder-[color:var(--asdk-dui-formfield-placeholder-text-color)] app-sdk-shadow-sm invalid:app-sdk-border-[color:var(--asdk-dui-formfield-invalid-color)] invalid:app-sdk-ring-1 invalid:app-sdk-ring-[color:var(--asdk-dui-formfield-invalid-color)] focus:app-sdk-border-[color:var(--asdk-dui-focus-color)] focus:app-sdk-ring-[color:var(--asdk-dui-focus-color)] sm:app-sdk-text-sm dark:app-sdk-border-solid dark:app-sdk-border-transparent dark:invalid:app-sdk-shadow-app-sdk-dark-invalid dark:focus:app-sdk-shadow-app-sdk-dark"
-  //         type="number"
-  //         step={0.01}
-  //         id={formField.id}
-  //         name={formField.id}
-  //         defaultValue={state || formField.defaultValue?.toString()}
-  //         placeholder={parsedCustomFormConfig?.placeholder || '0.00'}
-  //         aria-describedby={`${formField.id}-hint`}
-  //         data-form-field-type="number"
-  //       />
-  //     );
-  //     break;
-  //   default:
-  //     numberInput = (
-  //       <input
-  //         className="app-sdk-form-input app-sdk-text-app-sdk-inherit app-sdk-border app-sdk-py-2 app-sdk-px-3 app-sdk-block app-sdk-w-full app-sdk-rounded-md app-sdk-border-[color:var(--asdk-dui-border-color)] app-sdk-bg-[color:var(--asdk-dui-formfield-background-color)] app-sdk-placeholder-[color:var(--asdk-dui-formfield-placeholder-text-color)] app-sdk-shadow-sm invalid:app-sdk-border-[color:var(--asdk-dui-formfield-invalid-color)] invalid:app-sdk-ring-1 invalid:app-sdk-ring-[color:var(--asdk-dui-formfield-invalid-color)] focus:app-sdk-border-[color:var(--asdk-dui-focus-color)] focus:app-sdk-ring-[color:var(--asdk-dui-focus-color)] sm:app-sdk-text-sm dark:app-sdk-border-solid dark:app-sdk-border-transparent dark:invalid:app-sdk-shadow-app-sdk-dark-invalid dark:focus:app-sdk-shadow-app-sdk-dark"
-  //         type="number"
-  //         step={1}
-  //         id={formField.id}
-  //         name={formField.id}
-  //         defaultValue={state || formField.defaultValue?.toString()}
-  //         placeholder={parsedCustomFormConfig?.placeholder ?? '0'}
-  //         aria-describedby={`${formField.id}-hint`}
-  //         data-form-field-type="number"
-  //       />
-  //     );
-  //     break;
-  // }
 
   return (
     <div>
@@ -65,7 +29,6 @@ export function NumberFormField(props: DynamicUiComponentProps<string | null>, r
           aria-describedby={`${formField.id}-hint`}
           data-form-field-type="number"
         />
-        {/* {numberInput} */}
       </div>
       {parsedCustomFormConfig?.hint && (
         <p
