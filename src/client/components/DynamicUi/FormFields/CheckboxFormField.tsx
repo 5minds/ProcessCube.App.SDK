d0a9b387a9a851b38fd0fd501f2e59e6fdbd090d
Louisa Strunk <louisa.strunk@5minds.de>
2024-07-31 10:02:25 +0200

    change data-form-field-type of checkboxFormField to enum

--- src/client/components/DynamicUi/FormFields/CheckboxFormField.tsx
+++ src/client/components/DynamicUi/FormFields/CheckboxFormField.tsx
@@ -29,7 +29,7 @@ export function CheckboxFormField(
         className="app-sdk-pl-0 app-sdk-pb-0 app-sdk-space-y-2"
         data-type="checkbox"
         aria-describedby={parsedCustomFormConfig?.hint ? `${formField.id}-hint` : undefined}
-        data-form-field-type="checkbox"
+        data-form-field-type="enum"
       >
         {options?.map((option) => {
           const hasValueToBeChecked = multipleStateOrDefaultValue.find((value) => value.trim() === option.id);
