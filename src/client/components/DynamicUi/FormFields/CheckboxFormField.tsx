3528377cd2c95b07ec14c399457292c50947dacb
Louisa Strunk <louisa.strunk@5minds.de>
2024-08-01 16:02:21 +0200

    adjust rendering of checkbox formField

--- src/client/components/DynamicUi/FormFields/CheckboxFormField.tsx
+++ src/client/components/DynamicUi/FormFields/CheckboxFormField.tsx
@@ -33,6 +33,7 @@ export function CheckboxFormField(
         data-form-field-type="checkbox"
       >
         {options?.map((option: any) => {
+          console.log('option', option);
           const hasValueToBeChecked = multipleStateOrDefaultValue.find((value) => value.trim() === option.id);
 
           return (
@@ -42,17 +43,17 @@ export function CheckboxFormField(
                   type="checkbox"
                   defaultChecked={hasValueToBeChecked}
                   name={formField.id}
-                  id={option.id}
-                  value={option.id}
+                  id={option.key}
+                  value={option.key}
                   className="app-sdk-form-checkbox app-sdk-border app-sdk-h-4 app-sdk-w-4 app-sdk-rounded app-sdk-border-[color:var(--asdk-dui-border-color)] app-sdk-bg-[color:var(--asdk-dui-formfield-background-color)] app-sdk-text-[color:var(--asdk-dui-formfield-checkbox-text-color)] app-sdk-placeholder-[color:var(--asdk-dui-formfield-placeholder-text-color)] focus:app-sdk-border-[color:var(--asdk-dui-focus-color)] focus:app-sdk-ring-[color:var(--asdk-dui-focus-color)] dark:app-sdk-border-2 dark:app-sdk-border-solid dark:app-sdk-border-transparent dark:focus:app-sdk-shadow-app-sdk-dark"
                 />
               </div>
               <div className="app-sdk-ml-3 app-sdk-text-sm">
                 <label
-                  htmlFor={option.id}
+                  htmlFor={option.key}
                   className="app-sdk-font-medium app-sdk-text-[color:var(--asdk-dui-text-color)]"
                 >
-                  {option.name}
+                  {option.value}
                 </label>
               </div>
             </div>
