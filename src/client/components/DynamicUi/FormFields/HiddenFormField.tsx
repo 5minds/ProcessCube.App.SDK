eb612b5b96bde36634a276d852ec100665c2159d
Alexander Kasten <alexander.kasten@5minds.de>
2024-09-27 10:08:29 +0200

    Refactor HiddenFormField component to simplify structure and remove unnecessary div elements

--- src/client/components/DynamicUi/FormFields/HiddenFormField.tsx
+++ src/client/components/DynamicUi/FormFields/HiddenFormField.tsx
@@ -4,21 +4,15 @@ import { DynamicUiComponentProps } from '../DynamicUi';
 
 export function HiddenFormField(props: DynamicUiComponentProps<string | null>) {
   const { formField } = props;
-  const hintId = `${formField.id}-hint`;
 
   return (
-    <div>
-      <div className="app-sdk-mt-1">
-        <input
-          className="app-sdk-hidden"
-          type="hidden"
-          id={formField.id}
-          value={props.state || formField.defaultValue?.toString() || ''}
-          name={formField.id}
-          aria-describedby={hintId}
-          data-form-field-type="hidden"
-        />
-      </div>
-    </div>
+    <input
+      className="app-sdk-hidden"
+      type="hidden"
+      id={formField.id}
+      value={formField.defaultValue?.toString() || ''}
+      name={formField.id}
+      data-form-field-type="hidden"
+    />
   );
 }
