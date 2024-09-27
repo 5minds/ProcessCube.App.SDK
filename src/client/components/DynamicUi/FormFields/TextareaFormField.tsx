e0870354ed2c6700ca57c8f6b6d77324dffe4ff8
Alexander Kasten <alexander.kasten@5minds.de>
2024-09-27 10:08:37 +0200

    Refactor TextareaFormField classNames to use consistent naming conventions

--- src/client/components/DynamicUi/FormFields/TextareaFormField.tsx
+++ src/client/components/DynamicUi/FormFields/TextareaFormField.tsx
@@ -17,7 +17,7 @@ export function TextareaFormField(props: DynamicUiComponentProps<string>, ref: D
       </label>
       <div className="app-sdk-mt-1">
         <textarea
-          className="app-sdk-form-input app-sdk-text-app-sdk-inherit app-sdk-border app-sdk-py-2 app-sdk-px-3 app-sdk-block app-sdk-w-full app-sdk-rounded-md app-sdk-border-[color:var(--asdk-dui-border-color)] app-sdk-bg-[color:var(--asdk-dui-formfield-background-color)] app-sdk-placeholder-[color:var(--asdk-dui-formfield-placeholder-text-color)] app-sdk-shadow-sm focus:app-sdk-border-[color:var(--asdk-dui-focus-color)] focus:app-sdk-ring-[color:var(--asdk-dui-focus-color)] sm:app-sdk-text-sm dark:app-sdk-border-solid dark:app-sdk-border-transparent dark:focus:app-sdk-shadow-app-sdk-dark"
+          className="app-sdk-form-textarea app-sdk-text-app-sdk-inherit app-sdk-border app-sdk-py-2 app-sdk-px-3 app-sdk-block app-sdk-w-full app-sdk-rounded-md app-sdk-border-[color:var(--asdk-dui-border-color)] app-sdk-bg-[color:var(--asdk-dui-formfield-background-color)] app-sdk-placeholder-[color:var(--asdk-dui-formfield-placeholder-text-color)] app-sdk-shadow-sm focus:app-sdk-border-[color:var(--asdk-dui-focus-color)] focus:app-sdk-ring-[color:var(--asdk-dui-focus-color)] sm:app-sdk-text-sm dark:app-sdk-border-solid dark:app-sdk-border-transparent dark:focus:app-sdk-shadow-app-sdk-dark"
           defaultValue={defaultValue}
           id={formField.id}
           name={formField.id}
