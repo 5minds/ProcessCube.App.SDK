f6ceb73f02bc3b958edb7c178c26cff50eaccfaa
Matthias Danne <matthias.danne@web.de>
2025-04-16 12:54:17 +0200

    Migrate react-markdown to v10

--- src/client/components/DocumentationViewer.tsx
+++ src/client/components/DocumentationViewer.tsx
@@ -8,13 +8,14 @@ import remarkGfm from 'remark-gfm';
 export function DocumentationViewer(props: { documentation: string }) {
   return (
     <div className="app-sdk-absolute app-sdk-w-full app-sdk-h-full app-sdk-overflow-y-auto app-sdk-scroll-p-4 app-sdk-scroll-smooth app-sdk-scroll-shadow">
+      <div className="app-sdk-markdown lg:app-sdk-mx-40 md:app-sdk-mx-20 app-sdk-mx-10">
       <Markdown
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeRaw, rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]]}
-        className="app-sdk-markdown lg:app-sdk-mx-40 md:app-sdk-mx-20 app-sdk-mx-10"
       >
         {props.documentation ?? ''}
       </Markdown>
+      </div>
     </div>
   );
 }
