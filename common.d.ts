bb1d2b751dd3c411a3678a51d92e781a3db569e4
Matthias Danne <matthias.danne@web.de>
2026-01-27 15:25:26 +0100

    add missing ts deklaration files

---
+++ common.d.ts
@@ -0,0 +1,38 @@
+import { DefaultSession, User } from 'next-auth';
+import 'next-auth/jwt';
+
+export * from './build/common/index';
+
+/**
+ * Module augmentations for next-auth
+ * These extend the default session and JWT types to include claims from the SDK
+ */
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's identity claims. */
+      claims?: Record<string, unknown>;
+    } & DefaultSession['user'] & {
+        [profile_property: string]: any;
+      };
+    error?: 'RefreshAccessTokenError';
+  }
+}
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID Access Token */
+    accessToken?: string;
+    /** OpenID ID Token */
+    idToken?: string;
+    /** OpenID Refresh Token */
+    refreshToken?: string;
+    expiresAt: number;
+    error?: 'RefreshAccessTokenError';
+    user?: User;
+  }
+}
